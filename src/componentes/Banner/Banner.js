6d79c8aefa99b9014476848a2f0413553423030a
PedroMesquitaa <pedrozd45@gmail.com>
2024-11-01 11:34:53 -0300

    Ajustando o projeto para o meu gosto

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -2,8 +2,8 @@ import './Banner.css'
 
 function Banner() {
     return (
-        <header class="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        <header className="banner">
+            <img src="/imagens/CBLOL_2021_darkmode.png" alt="O banner principal da página do Projeto LOL"/>
         </header>
         
     )
