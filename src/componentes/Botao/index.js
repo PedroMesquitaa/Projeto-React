18a8f9269a484a90bd8dea81c531503bbeff4bb2
PedroMesquitaa <pedrozd45@gmail.com>
2024-11-04 12:02:18 -0300

    Feito o botao e lista

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,11 @@
+import './Botao.css'
+
+const Botao = (props) => {
+    return (
+        <button className='button-59'>
+            {props.children}
+        </button>
+    )
+}
+
+export default  Botao
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,35 @@
+import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+    const cor = [
+        'Azul',
+        'Amarelo',
+        'Laranja',
+        'Vermelho',
+        'Roxo',
+        'Verde'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido')
+    }
+
     return (
         <section className='formulario'>
-            <form>
-                <h2>Preencha os dados para criar o card do Jogadores</h2>
-                <CampoTexto label="Nome" placeholder="Digite o nome" />
-                <CampoTexto label="Lane" placeholder="Digite a lane" />
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card dos jogadores</h2>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o nome" />
+                <CampoTexto obrigatorio={true} label="Lane" placeholder="Digite a lane" />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Informe o endereço da imagem" />
+                <CampoTexto obrigatorio={true} label="Time" placeholder="Digite o nome do time" />
+                <ListaSuspensa obrigatorio={true} label="Cor" itens={cor} />
+                <Botao>
+                    Criar Card
+                </Botao>
             </form>
         </section>
     )
